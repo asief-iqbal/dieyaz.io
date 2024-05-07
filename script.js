4392b3c2df4bb9fb4437117f53044007c2408b5a
Asief Iqbal Dieyaz <147961056+asief-iqbal@users.noreply.github.com>
2024-05-08 00:35:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var speed = 100;
 
 function typeWriter() {
   if (i < txt[j].length) {
-    document.getElementById("cli").innerHTML += txt[j].charAt(i);
+    document.querySelector("#cli").innerHTML += txt[j].charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   } else {
-    if (document.getElementById("cli").innerHTML.length > 11) {
-      document.getElementById("cli").innerHTML = document
-        .getElementById("cli")
+    if (document.querySelector("#cli").innerHTML.length > 11) {
+      document.querySelector("#cli").innerHTML = document
+        .querySelector("#cli")
         .innerHTML.slice(0, -1);
       setTimeout(typeWriter, speed);
     } else {
@@ -24,19 +24,20 @@ function typeWriter() {
 
 typeWriter();
 
-var element = document.getElementById("your-element-id"); // replace 'your-element-id' with your element's id
+var element = document.querySelector("#your-element-id"); // replace 'your-element-id' with your element's id
 
 function updateBackground(e) {
-  const x = (e.clientX / element.offsetWidth) * 100;
-  const y = (e.clientY / element.offsetHeight) * 100;
-  element.style.background = `radial-gradient(circle at ${x}% ${y}%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)`;
+  const x = (e.clientX / window.innerWidth) * 100;
+  const y = (e.clientY / window.innerHeight) * 100;
+  document.body.style.background = `radial-gradient(circle at ${x}% ${y}%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)`;
 }
 
-element.addEventListener("mousemove", updateBackground);
-element.addEventListener("touchmove", function (e) {
-  var touch = e.touches[0];
-  var touchLocation = touch.target.getBoundingClientRect();
-  e.clientX = touch.clientX - touchLocation.left;
-  e.clientY = touch.clientY - touchLocation.top;
+document.addEventListener("mousemove", updateBackground);
+document.addEventListener("touchmove", function (e) {
+  e.clientX = e.touches[0].clientX;
+  e.clientY = e.touches[0].clientY;
+  updateBackground(e);
+  e.clientY = e.touches[0].clientY;
   updateBackground(e);
 });
+
